f9034d03a3ee90c7ee1c2a79ce3c0023aaa738df
Gareth Williams <6419944+Gaweph@users.noreply.github.com>
2022-02-04 11:13:05 +0000

    building project list correctly

--- src/prebuild/build-projects-list.js
+++ src/prebuild/build-projects-list.js
@@ -12,17 +12,15 @@ import showdown from 'showdown';
 export const BuildProjects = async () => {
 
     const githubUserName = "Gaweph";
+    const projectTag = 'project-tag';
     try {
         const response = await axios.get(`https://api.github.com/users/${githubUserName}/repos`)
-        const filtered = response.data.filter(x => x.topics.indexOf('project-tag') != -1);
+        const projects = response.data.filter(x => x.topics.indexOf(projectTag) != -1);
 
         const converter = new showdown.Converter();
-        // console.log(filtered.length);
-        let projects = [];
-        for(var i = 0; i < filtered.length; i++) 
+        for(var i = 0; i < projects.length; i++) 
         {
-            const item = filtered[i];
-
+            const item = projects[i];
             const readMeUrl = `https://raw.githubusercontent.com/${githubUserName}/${item.name}/master/README.md`;
 
             let readme = '';
@@ -36,7 +34,7 @@ export const BuildProjects = async () => {
                 // console.log("AAAA", readme, "BBBB");
 
                 // <pre><code class="typescript language-typescript"></code>
-                readme = readme.replace(new RegExp('<pre><code( class=".*")?', 'g'), '<Prism language="javascript" code={`')
+                readme = readme.replace(new RegExp('<pre><code( class=".*")?>', 'g'), '<Prism language="csharp" code={`')
                 // console.log(readme.indexOf('</code></pre>'));
                 readme = readme.replace(new RegExp('</code></pre>', 'g'), '`} />')
                 // console.log(readme.indexOf('</code></pre>'));
@@ -53,9 +51,16 @@ export const BuildProjects = async () => {
     import PostContents from "$components/Post-Contents.svelte";
     import Prism from "$components/PrismJS.svelte";
     import PostSection from "$components/Post-Section.svelte";
+
+    const title = "${item.name}";
+    const tags = ${JSON.stringify(item.topics.filter(x => x != projectTag))};
+    const projects = [];
+    
+    const date = '${new Date(item.created_at)}'
+
 </script>
 
-<PostTop title="${item.name}" tags=${item.topics.join(',')} />
+<!-- <PostTop {title} {tags} {projects} />-->
 <PostContents>
     <PostSection>
 ${readme}
@@ -66,10 +71,20 @@ ${readme}
             var projectFilePath = __dirname + `/../routes/Projects/${item.name}.svelte`;
             fs.writeFileSync(projectFilePath, projectContent,{encoding:'utf8',flag:'w'});
 
-            projects.push({id: item.id, name: item.name, readme: readme});
+            // projects.push({id: item.id, name: item.name, readme: readme, tags: item.topics, description: item.description});
         }
             
-        var indexFileContents = projects.map(x=>x.name).toString();
+const indexFileContents = `
+<script>
+    import PostLink from '$components/Post-Link.svelte';
+</script>
+
+${projects.map(x=>`
+<PostLink title="${x.name}" slug="/projects/${x.name}" tags="${x.topics.filter(x => x != projectTag).join(',')}">
+${x.description}
+</PostLink>
+`).toString()}
+`;
         var indexFilePath = __dirname + "/../routes/Projects/index.svelte";
         fs.writeFileSync(indexFilePath, indexFileContents,{encoding:'utf8',flag:'w'});
         console.log("Created", indexFilePath);
